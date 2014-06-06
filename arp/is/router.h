15a9c87288af54af67cf5f5299906ab7dd00f5a8
Douglas Afonso de Sousa Costa <ra104825@deep.lab.ic.unicamp.br>
2014-06-06 09:46:02 -0300

    criando router.cpp e router.h

---
+++ arp/is/router.h
@@ -0,0 +1,38 @@
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef AC_TLM_MEM_H_
+#define AC_TLM_MEM_H_
+
+//////////////////////////////////////////////////////////////////////////////
+
+// Standard includes
+// SystemC includes
+#include <systemc>
+// ArchC includes
+#include "ac_tlm_protocol.H"
+
+//////////////////////////////////////////////////////////////////////////////
+
+// using statements
+using tlm::tlm_transport_if;
+
+//////////////////////////////////////////////////////////////////////////////
+
+//#define DEBUG
+
+/// Namespace to isolate memory from ArchC
+namespace user
+{
+
+/// A TLM memory
+class ac_tlm_mem :
+  public sc_module,
+  public ac_tlm_transport_if // Using ArchC TLM protocol
+{
+public:
+  /// Exposed port with ArchC interface
+  sc_export< ac_tlm_transport_if > target_export;
+
+};
+};
+
--- arp/platforms/helloworld_mips.01/main.cpp
+++ arp/platforms/helloworld_mips.01/main.cpp
@@ -22,6 +22,7 @@ const char *archc_options="-abi -dy ";
 #include  <systemc.h>
 #include  "mips1.H"
 #include  "ac_tlm_mem.h"
+#include  "router.h"
 
 using user::ac_tlm_mem;
 
@@ -31,12 +32,13 @@ int sc_main(int ac, char *av[])
   //!  ISA simulator
   mips1 mips1_proc1("mips1");
   ac_tlm_mem mem("mem");
+  router router("router");
 
 #ifdef AC_DEBUG
   ac_trace("mips1_proc1.trace");
 #endif 
-
-  mips1_proc1.DM_port(mem.target_export);
+  mips1_proc1.DM_port(router.target_export);
+//  mips1_proc1.DM_port(mem.target_export);
 
   mips1_proc1.init(ac, av);
   cerr << endl;
